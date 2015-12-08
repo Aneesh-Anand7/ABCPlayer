ef2f3807fc2c5f10b46178159001eaca888fbb51
Leopoldo Calderas <leo2017@mit.edu>
2015-12-07 21:58:12 -0500

    makeheader implemented, working on tests

--- test/abc/sound/MakeHeaderTest.java
+++ test/abc/sound/MakeHeaderTest.java
@@ -1,5 +1,9 @@
 package abc.sound;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import org.junit.Test;
@@ -8,7 +12,7 @@ import org.junit.Test;
 public class MakeHeaderTest {
 
     @Test
-    public void testMakeHeaderTitle() {
+    public void testMakeHeaderFurElise() {
         Map<String, String> title = Music.parseHeader("X: 1 \n "
                 + "T:Bagatelle No.25 in A, WoO.59 \n"
                 + "C:Ludwig van Beethoven \n"
@@ -18,8 +22,154 @@ public class MakeHeaderTest {
                 + "L:1/16 \n"
                 + "Q:1/8=140 \n"
                 + "K:Am");
-        System.out.println(title);
-//        assertEquals(title, new Number(0.00000001));
+        assertEquals(4, title.size());
+        assertEquals("3/8", title.get("meter"));
+        assertEquals("1/16" , title.get("length"));
+        assertEquals("1/8=140", title.get("tempo"));
+        assertEquals("Am", title.get("key"));
     }
-
+    
+    
+    @Test
+    public void testMakeHeaderInvention() throws IOException {
+        File file = new File("sample_abc/invention.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/8" , title.get("length"));
+        assertEquals("1/4=70", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderLittleNight() throws IOException {
+        File file = new File("sample_abc/little_night_music.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/8" , title.get("length"));
+        assertEquals("1/4=140", title.get("tempo"));
+        assertEquals("G", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderAbc_song() throws IOException {
+        File file = new File("sample_abc/abc_song.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=100", title.get("tempo"));
+        assertEquals("D", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderPaddy() throws IOException {
+        File file = new File("sample_abc/paddy.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("6/8", title.get("meter"));
+        assertEquals("1/8" , title.get("length"));
+        assertEquals("1/8=200", title.get("tempo"));
+        assertEquals("D", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderPiece1() throws IOException {
+        File file = new File("sample_abc/piece1.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=140", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderPiece2() throws IOException {
+        File file = new File("sample_abc/piece2.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=200", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderPrelude() throws IOException {
+        File file = new File("sample_abc/prelude.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/16" , title.get("length"));
+        assertEquals("1/4=70", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderSample1() throws IOException {
+        File file = new File("sample_abc/sample1.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/8" , title.get("length"));
+        assertEquals("1/8=100", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderSample2() throws IOException {
+        File file = new File("sample_abc/sample2.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=200", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderSample3() throws IOException {
+        File file = new File("sample_abc/sample3.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=200", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderSample4() throws IOException {
+        File file = new File("sample_abc/sample4.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=200", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderScale() throws IOException {
+        File file = new File("sample_abc/scale.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=200", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
+    
+    @Test
+    public void testMakeHeaderWaxiesDargle() throws IOException {
+        File file = new File("sample_abc/waxies_dargle.abc");
+        Map<String, String> title = Music.parseHeaderFromFile(file);
+        assertEquals(4, title.size());
+        assertEquals("4/4", title.get("meter"));
+        assertEquals("1/4" , title.get("length"));
+        assertEquals("1/4=200", title.get("tempo"));
+        assertEquals("C", title.get("key"));
+    }
 }
