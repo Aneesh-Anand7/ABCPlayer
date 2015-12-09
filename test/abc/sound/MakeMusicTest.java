21df513cd10d50f5e9347ddff4effb975de2b79e
Leopoldo Calderas <leo2017@mit.edu>
2015-12-08 23:51:40 -0500

    working on repeats

--- test/abc/sound/MakeMusicTest.java
+++ test/abc/sound/MakeMusicTest.java
@@ -2,20 +2,47 @@ package abc.sound;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiUnavailableException;
 
 import org.junit.Test;
 
+import abc.parser.SplitHeader;
+
 public class MakeMusicTest {
 
     @Test
     public void testMakeMusicInvention() throws IOException, MidiUnavailableException, InvalidMidiDataException {
         File file = new File("sample_abc/invention.abc");
-        Music music = Music.parseBodyFromFile(file);
-        SequencePlayer player = new SequencePlayer(file);
+        List<String> headbody = SplitHeader.splitHeader(file);
+        System.out.println(headbody.get(1));
+        Map<String, String> header = Music.parseHeader(headbody.get(0));
+        System.out.println(header);
+        Music music = Music.parseBody(headbody.get(1),header);
+        System.out.println(music);
+        SequencePlayer player = new SequencePlayer(300);
         music.play(player, 0);
+        player.play();
+        System.in.read();
     }
 
+    @Test
+    public void testMakeMusicRepeat1() throws IOException, MidiUnavailableException, InvalidMidiDataException {
+        File file = new File("sample_abc/repeat1.abc");
+        List<String> headbody = SplitHeader.splitHeader(file);
+        System.out.println(headbody.get(1));
+        Map<String, String> header = Music.parseHeader(headbody.get(0));
+        System.out.println(header);
+        Music music = Music.parseBody(headbody.get(1),header);
+        System.out.println(music);
+        SequencePlayer player = new SequencePlayer(file);
+        music.play(player, 0);
+        player.play();
+        System.in.read();
+    }
+    
+        
 }
