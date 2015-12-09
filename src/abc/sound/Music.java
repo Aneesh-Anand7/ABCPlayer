e239b72816a07f4f0d7b87c33e80d70da0a1564d
Nicole Lu <nicolelu@mit.edu>
2015-12-09 03:59:54 -0500

    testing

--- src/abc/sound/Music.java
+++ src/abc/sound/Music.java
@@ -154,7 +154,7 @@ public interface Music {
     public boolean isNote();
 
     public static void main(String[] args) throws IOException, MidiUnavailableException, InvalidMidiDataException {
-        File file = new File("sample_abc/musicBeforeVoiceTest.abc");
+        File file = new File("sample_abc/sample2.abc");
         List<String> headbody = SplitHeader.splitHeader(file);
         System.out.println(headbody.get(1));
         Map<String, String> header = parseHeader(headbody.get(0));
